ba6e5f207b9f107eaedb1efebba1498fe2ecaac7
AlexDawn <gallardonicolas.8100@outlook.com>
2024-09-06 11:09:37 -0300

    fixed included libraries

--- GallardonNicolas-TP2/src/main.cpp
+++ GallardonNicolas-TP2/src/main.cpp
@@ -1,6 +1,20 @@
 #include "raylib.h"
 
-int main(int argc, char* argv[])
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+int main()
 {
-    return 0;
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "TP2");
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLANK);
+
+        DrawRectangle(0, 0, 90, 90, WHITE);
+
+        EndDrawing();
+    }
+
 }
