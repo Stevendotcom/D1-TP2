513ae2226b99d9cce33f1f68a80b56380af8e9b7
AlexDawn <gallardonicolas.8100@outlook.com>
2024-09-06 12:11:35 -0300

    reconfig for sigil

--- GallardonNicolas-TP2/src/main.cpp
+++ GallardonNicolas-TP2/src/main.cpp
@@ -1,20 +1,32 @@
-#include "raylib.h"
+#include "sl.h"
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-
-int main()
+int main(int args, char* argv[])
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "TP2");
-    SetTargetFPS(60);
-    while (!WindowShouldClose())
+    // set up our window and a few resources we need
+    slWindow(400, 400, "Simple SIGIL Example", false);
+    slSetFont(slLoadFont("ttf/white_rabbit.ttf"), 24);
+    slSetTextAlign(SL_ALIGN_CENTER);
+    int tex = slLoadTexture("png/glow.png");
+
+    while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
     {
-        BeginDrawing();
-        ClearBackground(BLANK);
+        // background glow
+        slSetForeColor(0.1, 0.9, 0.2, 0.4);
+        slSprite(tex, 200, 240, 300, 200);
 
-        DrawRectangle(0, 0, 90, 90, WHITE);
+        // large text and fat line
+        slSetForeColor(0.0, 0.8, 0.2, 1.0);
+        slSetFontSize(24);
+        slText(200, 250, "SIGIL:");
+        slRectangleFill(200, 240, 100, 7);
 
-        EndDrawing();
+        // smaller subtext
+        slSetFontSize(14);
+        slText(200, 220, "Sprites, text, sound, input, and more!");
+        slLine(48, 210, 352, 210);
+
+        slRender(); // draw everything
     }
-
+    slClose(); // close the window and shut down SIGIL
+    return 0;
 }
