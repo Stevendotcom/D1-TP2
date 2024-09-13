331afbd8b40eab284fd6b4bb6ece6d1d85654be3
AlexGallardo <gallardonicolas.8100@outlook.com>
2024-09-13 11:31:37 -0300

    finished button functions

--- GallardonNicolas-TP2/src/actors/Buttons.cpp
+++ GallardonNicolas-TP2/src/actors/Buttons.cpp
@@ -1,1 +1,27 @@
 ﻿#include "Buttons.h"
+
+extern const std::string assetsDir;
+const Wrapper::Font buttonFont = Wrapper::LoadFont(assetsDir + "DirtyHarold_Font/DirtyHarold");
+const int margin = 3;
+
+void Buttons::RenderButtons(Button buttons[], const int amount)
+{
+    for(int i = 0; i < amount; i++)
+    {
+        Wrapper::LoadSprite(buttons[i].Sprite, buttons[i].Position, buttons[i].Size);
+
+        if(buttons[i].Selected)
+        {
+            Wrapper::SetForeColor(buttons[i].ColorSelected, 1.0f);
+        }
+        else
+        {
+            Wrapper::SetForeColor(buttons[i].Color, 1.0f);
+        }
+
+        Wrapper::CenterText();
+        Wrapper::ChangeFontSize(buttons[i].Size.Y - margin);
+        Wrapper::SetFont(buttonFont);
+        Wrapper::TextPrint({buttons[i].Position.X, buttons[i].Position.Y + margin}, buttons[i].Text);
+    }
+}
--- GallardonNicolas-TP2/src/actors/Buttons.h
+++ GallardonNicolas-TP2/src/actors/Buttons.h
@@ -1,7 +1,18 @@
 ﻿#pragma once
 
+#include "Wrapper.h"
+
 namespace Buttons
 {
-
-    
+    struct Button
+    {
+        Wrapper::Vector2 Position;
+        Wrapper::Vector2 Size;
+        Wrapper::Texture Sprite;
+        Wrapper::Color Color;
+        Wrapper::Color ColorSelected;
+        std::string Text;
+        bool Selected;
+    };
+    void RenderButtons(Button buttons[], int amount);
 };
