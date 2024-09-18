51f5ec440ed7f89a6bf1d559754a766c7aedf9b6
AlexGallardo <gallardonicolas.8100@outlook.com>
2024-09-18 10:08:08 -0300

    Player done

--- GallardonNicolas-TP2/GallardonNicolas-TP2/Player.cpp
+++ GallardonNicolas-TP2/GallardonNicolas-TP2/Player.cpp
@@ -1,1 +1,41 @@
 #include "Player.h"
+
+#include <cfloat>
+
+// definitions
+
+void Player::MoveRight(Player &player)
+{
+    player.FuturePosition.X += player.Speed * Wrapper::GetFrameTime();
+}
+
+void Player::MoveLeft(Player &player)
+{
+    player.FuturePosition.X -= player.Speed * Wrapper::GetFrameTime();
+}
+
+bool Player::DoesCollide(Player &player)
+{
+    return player.FuturePosition.X <= FLT_EPSILON ||
+        player.FuturePosition.X >= static_cast<float>(SCREEN_HEIGHT - player.Width) - FLT_EPSILON;
+}
+
+void Player::Update(Player &p1)
+{
+    if (!DoesCollide(p1))
+    {
+        p1.Position.X = p1.FuturePosition.y;
+    }
+    p1.FuturePosition.X = p1.Position.X; // reset fpositions
+
+}
+
+void Player::Spawn(Player &player, Wrapper::Texture sprite)
+{
+    player.Height = HEIGHT;
+    player.Width = WIDTH;
+    player.Speed = SPEED;
+    player.Position = {static_cast<float>(SCREEN_WIDTH) / 2.0f, MARGIN};
+    player.FuturePosition = player.Position;
+    player.Sprite = sprite;
+}
--- GallardonNicolas-TP2/GallardonNicolas-TP2/Player.h
+++ GallardonNicolas-TP2/GallardonNicolas-TP2/Player.h
@@ -1,4 +1,47 @@
 #pragma once
+#include "Wrapper.h"
+
+extern const int SCREEN_WIDTH;
+extern const int SCREEN_HEIGHT;
+
 namespace Player
 {
-};
+    const int MARGIN = 10;
+    const int SPEED = 600;
+    const int WIDTH = 25;
+    const int HEIGHT = 150;
+
+    enum class WhereCollides
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        None
+    };
+    struct Key
+    {
+        int Left;
+        int Right;
+    };
+
+    struct Player
+    {
+        Wrapper::Vector2 Position;
+        Wrapper::Vector2 FuturePosition;
+        Wrapper::Texture Sprite;
+        Key Keys;
+        float Direction;
+        int Width;
+        int Height;
+        int Speed;
+        int Score;
+    };
+
+
+    void MoveRight(Player &player);
+    void MoveLeft(Player &player);
+    bool DoesCollide(Player &player);
+    void Update(Player &p1);
+    void Spawn(Player &player, Wrapper::Texture sprite);
+} 
