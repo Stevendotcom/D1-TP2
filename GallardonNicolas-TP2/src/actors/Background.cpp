d3954e935af215b2b620fcdeaefdd2ba0c9fd0fe
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-09-21 17:30:48 -0300

    Bacground Render

---
+++ GallardonNicolas-TP2/src/actors/Background.cpp
@@ -0,0 +1,11 @@
+﻿#include "Background.h"
+
+#include "ColorPalette.h"
+#include "Sprites.h"
+
+void Background::Draw()
+{
+    SetForeColor(Color::white, 1.0);
+    Sprites::LoadSprite(sprites.Background, {SCREEN_WIDTH / 2.0F, SCREEN_HEIGHT / 2.0F}, {
+                                     static_cast<float>(SCREEN_WIDTH), static_cast<float>(SCREEN_HEIGHT)});
+}
