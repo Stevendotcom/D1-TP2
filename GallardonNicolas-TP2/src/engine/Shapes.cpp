12eca002b633763aa60c956f1833bad88d434092
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-09-22 21:58:12 -0300

    added shapes to engine

---
+++ GallardonNicolas-TP2/src/engine/Shapes.cpp
@@ -0,0 +1,23 @@
+﻿#include "Shapes.h"
+
+#include <sl.h>
+
+void Shapes::DrawRect(const VectorMath::Vector2 &position, const VectorMath::Vector2 &size)
+{
+    slRectangleFill(position.X, position.Y, size.X, size.Y);
+}
+
+void Shapes::DrawRectOutline(const VectorMath::Vector2 &position, const VectorMath::Vector2 &size)
+{
+    slRectangleOutline(position.X, position.Y, size.X, size.Y);
+}
+
+void Shapes::DrawCircle(const VectorMath::Vector2 &position, float radius, Color::Color &color, float opacity)
+{
+    slCircleFill(position.X,position.Y,radius, 64);
+}
+
+void Shapes::DrawCircleOutline(const VectorMath::Vector2 &position, float radius, Color::Color &color, float opacity)
+{
+    slCircleOutline(position.X,position.Y,radius, 64);
+}
---
+++ GallardonNicolas-TP2/src/engine/Shapes.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "ColorPalette.h"
+#include "VectorMath.h"
+
+namespace Shapes
+{
+    void DrawRect(const VectorMath::Vector2& position, const VectorMath::Vector2& size);
+    void DrawRectOutline(const VectorMath::Vector2& position, const VectorMath::Vector2& size);
+    void DrawCircle(const VectorMath::Vector2& position, float radius, Color::Color& color, float opacity);
+    void DrawCircleOutline(const VectorMath::Vector2& position, float radius, Color::Color& color, float opacity);
+
+};
